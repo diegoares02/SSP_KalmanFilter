ef7c4960ae2cf36583288fdaac22b14976573c78
diegoares02 <diegoares02@gmail.com>
2018-11-14 18:36:56 -0400

    new file:   Kalman.m 	new file:   amerhisData.mat

--- gen_Kalman_coefs.m
+++ gen_Kalman_coefs.m
@@ -1,27 +1,25 @@
 function [kph,kf]=gen_Kalman_coefs(R,Q,S0,F,G,H,N)
-K=zeros(N,2);
+kph=zeros(1,N);
+kf=zeros(1,N);
 S=S0;
 for n=0:N-1
     S0=(F*S*transpose(F))+Q; %Covariance Matrix of the prediction error
     P=S0*transpose(H)*(R+(H*S0*transpose(H)))^-1;%Kalman Gains    
     S=(eye(2)-P*H)*S0;
-    K(n+1,1)=P(1);
-    K(n+1,2)=P(2);
+    kph(n+1)=P(1);
+    kf(n+1)=P(2);
 end
-display(K)
 figure
 ax1=subplot(2,1,1);
-plot(K(:,1))
+plot(kph(1,:))
 title(ax1,'Phase Gain');
 ylabel(ax1,'KPH');
 xlabel(ax1,'N');
 grid on;
 ax2=subplot(2,1,2);
-plot(K(:,2))
+plot(kf(1,:))
 title(ax2,'Frequency Gain')
 ylabel(ax2,'KF')
 xlabel(ax2,'N');
 grid on;
-kph=P(1);
-kf=P(2);
 end
